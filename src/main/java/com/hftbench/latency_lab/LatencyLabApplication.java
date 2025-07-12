ca6e72ad4850d04607328b25f8f25f90305697c9
DESKTOP-E69NCPT\Owner <luke@weston.net.nz>
2025-07-12 10:10:54 +0100

    Initial commit

---
+++ src/main/java/com/hftbench/latency_lab/LatencyLabApplication.java
@@ -0,0 +1,13 @@
+package com.hftbench.latency_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatencyLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatencyLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hftbench/latency_lab/LatencyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hftbench.latency_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatencyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
